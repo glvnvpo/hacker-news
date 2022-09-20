1bbfbe58ee65ee4197b8263fb5151d5270757895
Polina Golovanova <polin-mart@ya.ru>
2022-09-20 17:53:56 +0300

    Add calling new stories requests every minute and cancelling new stories requests

--- src/pages/SingleStory/index.js
+++ src/pages/SingleStory/index.js
@@ -1,30 +1,3 @@
 // @flow
 
-import React, { useState, useEffect }  from 'react';
-import {useParams} from "react-router-dom";
-import axios from 'axios';
-import {ITEM} from "../../api/constants";
-
-type Props = {
-
-}
-
-export const SingleStory = (props: Props) => {
-	const {id} = useParams();
-
-	const [story, setStory] = useState({});
-	
-	useEffect(()=>{
-		console.log('on mount single');
-		loadStory(id);
-	}, [id]);
-
-	const loadStory = (id: number | string) => {
-		axios(ITEM(id)).then(({data})=>{
-			console.log(data);
-			setStory(data);
-		});
-	};
-
-	return (<div>SINGLE STORY PAGE {story.title}</div>);
-};
+export {SingleStory} from './SingleStory';
