f8b06af78d28257e23976b7e38529bfd02bf2e91
Polina Golovanova <polin-mart@ya.ru>
2022-10-02 19:28:11 +0300

    Add layout for Single Story page

--- src/pages/SingleStory/SingleStory.jsx
+++ src/pages/SingleStory/SingleStory.jsx
@@ -2,9 +2,11 @@
 
 import React, { useState, useEffect }  from 'react';
 import {useParams, useNavigate, useLocation} from "react-router-dom";
+import {Button, Card} from "react-bootstrap";
+import parse from 'html-react-parser';
 import axios from 'axios';
 import {isEmpty, isNull} from "lodash";
-import {Button} from "react-bootstrap";
+import './styles.scss';
 import {ITEM} from "../../api/constants";
 import {MINUTE} from "../../constants/time";
 import {MAIN_PAGE_PATH} from "../../routing/constants";
@@ -17,6 +19,8 @@ type Story = {
 	time: number | string;
 	score: number | string;
 	kids?: Array<number | string>;
+	text?: string;
+	descendants?: number | string;
 }
 
 type Comment = {
@@ -26,6 +30,7 @@ type Comment = {
 	time: number | string;
 	kids?: Array<number | string>;
 	children?: Array<Comment>;
+	showChildComment?: boolean;
 }
 
 export const SingleStory = () => {
@@ -39,13 +44,13 @@ export const SingleStory = () => {
 
 	let timer;
 
-	useEffect(()=>{
+	useEffect(() => {
 		updateStoryAndCommentsEachMinute(id);
 		return () => clearTimeout(timer);
 	}, [location?.pathname]);
 
-	const loadStory = (id: number | string) => {
-		return new Promise((resolve)=> {
+	const loadStory = (id: number | string): Promise<Story | string> => {
+		return new Promise((resolve) => {
 			axios(ITEM(id))
 				.then(({data}) => {
 					setStory(data);
@@ -81,7 +86,7 @@ export const SingleStory = () => {
 		}
 	};
 
-	const loadChildrenComments = (comment: Comment) => {
+	const loadChildrenComments = (comment: Comment): Promise<Comment> => {
 		const {id, kids} = comment || {};
 
 		return new Promise((resolve) => {
@@ -89,32 +94,91 @@ export const SingleStory = () => {
 				const promises = kids.map(loadOneComment);
 				Promise.all(promises)
 					.then(childrenComm =>
-						resolve({...comment, children: childrenComm})
+						resolve({...comment, children: childrenComm, showChildComment: false})
 					);
 			}
 			else resolve(comment);
 		});
 	};
 
-	const loadOneComment = (id: string | number) => {
-		return new Promise((resolve, reject)=> {
+	const loadOneComment = (id: string | number): Promise<Comment | string> => {
+		return new Promise((resolve, reject) => {
 			axios(ITEM(id))
 				.then(({data}) => !isNull(data) && resolve(data))
 				.catch(err => reject(err));
 		});
 	};
 
+	const changeVisibilityOfChildComment = (parentId: number | string) => {
+		const parentComment = comments.filter(({id}) => parentId === id)[0];
+		const newVisibility = !parentComment.showChildComment;
+		const newComments = comments.map(
+			(comment) => comment.id === parentId ?
+				{...parentComment, showChildComment: newVisibility}
+				: comment
+		);
+		
+		setComments(newComments);
+	};
+
+	let {title, score, by, time, url, descendants, text} = story || {};
+
 	return (
-		<div>SINGLE STORY PAGE
-			<Button onClick={()=>goBackToStories()} variant="outline-primary">Go back to stories</Button>
-			{story.title}
-			<Button onClick={() => updateStoryAndComments(id)} variant="outline-success">Update comments</Button>
-			{!isEmpty(comments) && comments.map(({id, by, text, time, children}: Comment) =>
-				<div key={id}><span onClick={()=>{}}><b>{by}</b>{getDateFromTimestamp(time)} {text}</span>
-					<button onClick={()=>loadChildrenComments(id, kids)}>Download children comments</button>
-					<div> <b>Children comments:</b></div>
-					{!isEmpty(children) && children.map(({id, by, text, time}) => <div key={id} style={{background: 'lightpink'}}><b>{by}</b> {getDateFromTimestamp(time)} {text}</div> )}
+		<div className="single-story">
+			<div className='content'>
+
+				<Button className="mt-20 mb-10" onClick={() => goBackToStories()} variant="outline-primary">Go back to news</Button>
+
+				<Card className='story mt-10 mb-20'>
+					<Card.Body>
+						<Card.Title>{title}</Card.Title>
+						<Card.Subtitle className="mb-2">{score} points | {by} | {getDateFromTimestamp(time)}</Card.Subtitle>
+						<Card.Subtitle className="mb-2">
+							{url ? <a href={url} target="_blank" rel="noreferrer">Source</a> : 'No source'}
+						</Card.Subtitle>
+						<Card.Subtitle className="mb-2">Comment count: {descendants}</Card.Subtitle>
+						{ text ?
+							<Card.Text as='span' className="bold">
+								{parse(text)}
+							</Card.Text>
+							: <span className='empty bold'>To read the text, visit the source</span>}
+					</Card.Body>
+				</Card>
+
+				<Button onClick={() => updateStoryAndComments(id)} variant="outline-success">Update comments</Button>
+
+				<div className='comments mt-20'>
+					{!isEmpty(comments) ? <h6 className='title'>Comments:</h6> : <h6 className='empty'>No comments found</h6>}
+
+					{!isEmpty(comments) && comments.map(({id, by, text, time, children, showChildComment}: Comment = {}) =>
+						<div className='comment-wrapper mt-20' key={id}>
+							{ by && <Card className='parent' border="primary">
+								<Card.Body>
+									<Card.Subtitle className="mb-2"><span className="bold">{by}</span> | {getDateFromTimestamp(time)}</Card.Subtitle>
+									<Card.Text className="bold" as='span'>
+										{text && parse(text)}
+									</Card.Text>
+									{ !isEmpty(children) &&
+										<Button onClick={() => changeVisibilityOfChildComment(id)} className="mt-20" variant="outline-secondary">+{children.length} answer{children.length > 1 && 's'}</Button>
+									}
+								</Card.Body>
+							</Card>
+							}
+
+
+							{ (!isEmpty(children) && showChildComment) && children.map(({id, by, text, time} = {}) =>
+								<Card key={id} className='child' border="success">
+									<Card.Body>
+										<Card.Subtitle className="mb-2"><span className="bold">{by}</span> | {getDateFromTimestamp(time)}</Card.Subtitle>
+										<Card.Text className="bold" as='span'>
+											{text && parse(text)}
+										</Card.Text>
+									</Card.Body>
+								</Card>
+							)}
+						</div>
+					)}
 				</div>
-			)}
+			</div>
 		</div>);
 };
