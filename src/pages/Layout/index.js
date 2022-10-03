3a72b8d8bab602d18521733a5f7f42aea4c700fb
Polina Golovanova <polin-mart@ya.ru>
2022-10-03 20:25:57 +0300

    Add adaptive layout

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,16 +1,3 @@
 // @flow
 
-import React from 'react';
-import {Outlet} from 'react-router-dom';
-import './styles.scss';
-
-export const Layout = () => {
-	return (
-		<>
-			<div className='layout'>
-				<h2>HACKER NEWS</h2>
-			</div>
-			<Outlet/>
-		</>
-	);
-};
+export {Layout} from './Layout';
