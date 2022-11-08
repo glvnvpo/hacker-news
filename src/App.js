4da41bb7f30e88f6b47e14aaa902509e4ecc3667
Polina Golovanova <polin-mart@ya.ru>
2022-11-09 01:07:56 +0300

    Change location of Layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
-import {Main, Layout, SingleStory} from "./pages";
+import {Main, SingleStory} from "./pages";
 import './App.scss';
+import {Layout} from "./components/Layout";
 import {MAIN_PAGE_PATH, ROOT_PATH, SINGLE_STORY_PAGE_PATH} from "./routing/constants";
 
 export const  App = () => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,2 @@
-export {Layout} from './Layout';
 export {Main} from './Main';
 export {SingleStory} from './SingleStory';
