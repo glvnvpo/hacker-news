aa842f71a68ebafdcb6cc0545d56088cb2ceb7ef
Polina Golovanova <polin-mart@ya.ru>
2022-11-19 23:50:16 +0300

    Refactor api/constants to TypeScript

--- src/api/constants.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-type ID = number | string;
-
-const BASE = 'https://hacker-news.firebaseio.com/v0';
-
-export const NEW_STORIES = () => `${BASE}/newstories.json`;
-export const ITEM = (id: ID)=> `${BASE}/item/${id}.json`;
