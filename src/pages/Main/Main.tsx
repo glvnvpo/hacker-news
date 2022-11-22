698dccefee24c50f99a16332c0e5604f0b505e9c
Polina Golovanova <polin-mart@ya.ru>
2022-11-22 17:29:11 +0300

    Refactor Main page to TypeScript

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,43 +1,43 @@
 // @flow
 
-import React, {useEffect, useState} from 'react';
-import {useLocation} from "react-router-dom";
-import {useSelector, useDispatch} from 'react-redux';
+import React, {FC, useEffect, useState, MouseEvent} from 'react';
+import {useLocation} from 'react-router-dom';
 import {Button, Spinner} from 'react-bootstrap';
 import axios from 'axios';
 import {isEmpty, isNull} from 'lodash';
 import './styles.scss';
 import type {Story, ID} from '../../types';
 import {setStories} from '../../store/stories';
-import {ITEM, NEW_STORIES} from "../../api/constants";
-import {MINUTE} from "../../constants/time";
+import {ITEM, NEW_STORIES} from '../../api/constants';
+import {MINUTE} from '../../constants/time';
 import {MAIN_PAGE_PATH} from '../../routing/constants';
-import {StoryCard} from "../../components/StoryCard";
+import {StoryCard} from '../../components/StoryCard';
+import {useAppDispatch, useAppSelector} from '../../hooks/redux';
 
 const STORIES_NUMBER = 100;
 
-export const Main = () => {
+export const Main: FC = () => {
 
-	const stories = useSelector(state => state.stories.value);
-	const dispatch = useDispatch();
+	const stories = useAppSelector(state => state.stories.value);
+	const dispatch = useAppDispatch();
 
-	const [isLoading, setLoading] = useState(stories.length <= 0);
+	const [isLoading, setLoading] = useState<boolean>(stories.length <= 0);
 
 	let location = useLocation();
 
-	let timer;
+	let timer: NodeJS.Timeout | undefined;
 
 	useEffect(() => {
 		loadStoriesEachMinute();
 		return () => clearTimeout(timer);
 	}, [location?.pathname]);
 
-	const loadStoriesEachMinute = () => {
+	const loadStoriesEachMinute = ():void => {
 		loadNewStories();
 		timer = setTimeout(loadStoriesEachMinute, MINUTE);
 	};
 
-	const loadNewStories = (event = undefined) => {
+	const loadNewStories = (event?: MouseEvent):void => {
 
 		if (event) {
 			setLoading(true);
@@ -46,13 +46,14 @@ export const Main = () => {
 		axios<ID[]>(NEW_STORIES())
 			.then(async ({data}) => {
 				const newestStories = [...data.slice(0, STORIES_NUMBER)];
-				let loadedStories = [];
+				let loadedStories: Story[] = [];
 				const promises = newestStories.map(loadOneStory);
 
 				await Promise.allSettled(promises)
 					.then(data =>
 						loadedStories = data
-							.filter(({status})=> status === 'fulfilled')
+							.filter(({status}) => status === 'fulfilled')
+							// @ts-ignore
 							.map(({value}) => value)
 					)
 					.finally(() => {
@@ -64,7 +65,7 @@ export const Main = () => {
 	};
 
 	const loadOneStory = (id: ID) => {
-		return new Promise((resolve, reject)=> {
+		return new Promise((resolve, reject) => {
 			axios<Story>(ITEM(id))
 				.then(({data}) => !isNull(data) ? resolve(data) : reject())
 				.catch(err => reject(err));
@@ -72,15 +73,15 @@ export const Main = () => {
 	};
 
 	return (
-		<div className="main">
+		<div className='main'>
 			<div className='content'>
 				<div className='header bg-white'>
-					<h4 className="color-grey2">Latest news</h4>
-					<Button onClick={(e) => loadNewStories(e)} variant="outline-success">Update news</Button>
+					<h4 className='color-grey2'>Latest news</h4>
+					<Button onClick={(e) => loadNewStories(e)} variant='outline-success'>Update news</Button>
 				</div>
 
 				<div className='cards mt-10'>
-					{isLoading ? <Spinner animation="border" variant="secondary" className="mt-20" /> :
+					{isLoading ? <Spinner animation='border' variant='secondary' className='mt-20' /> :
 						(!isEmpty(stories)) ? stories.map((story: Story) =>
 							<StoryCard
 								story={story}
@@ -89,7 +90,7 @@ export const Main = () => {
 								to={`${MAIN_PAGE_PATH}/${story.id}`}
 								extraFieldsToShow={[]}
 							/>
-						) : <h5 className="mt-20">No stories found :(</h5>
+						) : <h5 className='mt-20'>No stories found :(</h5>
 					}
 				</div>
 
