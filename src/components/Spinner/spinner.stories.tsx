f6c6614044cbdebbf2f9ffc982d71e005cf0aa9b
Polina Golovanova <polin-mart@ya.ru>
2023-01-10 19:07:21 +0300

    Add Spinner component to Storybook

---
+++ src/components/Spinner/spinner.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Spinner} from './index';
+import {Sizes, Variants} from './spinner';
+
+export default {
+	title: 'Spinner',
+	component: Spinner,
+	argTypes: {
+		variant: {
+			defaultValue: Variants.SECONDARY,
+			options: [Variants.PRIMARY, Variants.SECONDARY]
+		},
+		size: {
+			options: [Sizes.SM],
+			control: {
+				type: 'check',
+				labels: {
+					[Sizes.SM]: 'small'
+				}
+			}
+		}
+	}
+} as ComponentMeta<typeof Spinner>;
+
+const Template: ComponentStory<typeof Spinner> = (args) => <Spinner {...args} />;
+
+export const Default = Template.bind({});
+
+export const Primary = Template.bind({});
+Primary.args = {variant: Variants.PRIMARY};
+
+export const Small = Template.bind({});
+Small.args = {size: Sizes.SM};
